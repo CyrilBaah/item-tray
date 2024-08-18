beb6ab9bfa7d8014fa80e51e44971a15a379016d
CyrilBaah <baah_yard12@yahoo.com>
2024-08-18 19:31:25 +0000

    manifest file and sample commands

--- kubectl exec -it postgres-6c8ddbfcc4-jnx.sh
+++ kubectl exec -it postgres-6c8ddbfcc4-jnx.sh
@@ -1,16 +1,16 @@
 kubectl exec -it postgres-77b9699b65-955bk -- psql -U postgres
 
 
-kubectl exec -it postgres-77b9699b65-955bk -- psql -U postgres
-psql -h localhost -U datadog postgres -A \
+kubectl exec -it postgres-77b9699b65-2bwfw -- psql -U postgres
+psql -h my-rds.eu-west-1.rds.amazonaws.com -U datadog postgres -A \
   -c "select * from pg_stat_database limit 1;" \
   && echo -e "\e[0;32mPostgres connection - OK\e[0m" \
   || echo -e "\e[0;31mCannot connect to Postgres\e[0m"
-psql -h localhost -U datadog postgres -A \
+psql -h my-rds.eu-west-1.rds.amazonaws.com -U datadog postgres -A \
   -c "select * from pg_stat_activity limit 1;" \
   && echo -e "\e[0;32mPostgres pg_stat_activity read OK\e[0m" \
   || echo -e "\e[0;31mCannot read from pg_stat_activity\e[0m"
-psql -h localhost -U datadog postgres -A \
+psql -h my-rds.eu-west-1.rds.amazonaws.com -U datadog postgres -A \
   -c "select * from pg_stat_statements limit 1;" \
   && echo -e "\e[0;32mPostgres pg_stat_statements read OK\e[0m" \
   || echo -e "\e[0;31mCannot read from pg_stat_statements\e[0m"
@@ -31,7 +31,41 @@ instances:
 EOL
 
 ```sh
-kubectl cp /Users/cyril/Desktop/workspace/apps/item-tray/ops/db/conf.yaml datadog-agent-kdhsb:/etc/datadog-agent/conf.d/postgres.d/conf.yaml
+kubectl cp apps/item-tray/ops/db/conf.yaml datadog-agent-kdhsb:/etc/datadog-agent/conf.d/postgres.d/conf.yaml
 ```
 
-kubectl exec -it datadog-agent-kdhsb -- psql -h postgres -U datadog -d postgres -c "SELECT * FROM pg_stat_database LIMIT 1;"
+kubectl exec -it postgres-774b697bd5-fzshk -- psql -h postgres -U datadog -d postgres -c "SELECT * FROM pg_stat_database LIMIT 1;"
+
+
+# Connect to DB via kubernetes cluster
+```sh
+ kubectl exec -it postgres-dd9d647d8-l6wlg -- psql -U postgres -d postgres
+```
+
+kubectl exec -it postgres-77b9699b65-2bwfw -- psql -U postgres -c "SELECT * FROM pg_available_extensions WHERE name = 'pg_stat_statements';"
+
+
+# check if pg_stat_statements is installed
+```sh
+SELECT * 
+FROM pg_available_extensions 
+WHERE 
+    name = 'pg_stat_statements' and 
+    installed_version is not null;
+```
+alter system set shared_preload_libraries='pg_stat_statements';
+
+psql -h postgres-77b9699b65-2bwfw -U datadog postgres -A \
+  -c "select * from pg_stat_database limit 1;" \
+  && echo -e "\e[0;32mPostgres connection - OK\e[0m" \
+  || echo -e "\e[0;31mCannot connect to Postgres\e[0m"
+psql -h localhost -U datadog postgres -A \
+  -c "select * from pg_stat_activity limit 1;" \
+  && echo -e "\e[0;32mPostgres pg_stat_activity read OK\e[0m" \
+  || echo -e "\e[0;31mCannot read from pg_stat_activity\e[0m"
+psql -h localhost -U datadog postgres -A \
+  -c "select * from pg_stat_statements limit 1;" \
+  && echo -e "\e[0;32mPostgres pg_stat_statements read OK\e[0m" \
+  || echo -e "\e[0;31mCannot read from pg_stat_statements\e[0m"
+
+  CREATE USER datadog WITH password 'password';
--- ops/aws/aws_commands.md
+++ ops/aws/aws_commands.md
@@ -38,3 +38,20 @@ psql -h localhost -U myuser -d mydatabase
 kubectl create secret generic datadog-secret --from-literal api-key=cf01e4df2da6756e701c11dae4cebb17 --from-literal app-key=10af1674d99860a89d4bcd847a6b8f8b8a7b0750
 
 az aks get-credentials --name "aksdemo" --resource-group "RG1"
+
+psql -h postresdb.c520ooaic8e0.eu-west-1.rds.amazonaws.com -d postgres -U postgres
+
+CREATE USER datadog WITH password 'password';
+
+echo -n 'bX3bH9LDtFDqxpkY48Df' | base64 -d
+echo -n 'bX3bH9LDtFDqxpkY48Df' | base64
+
+
+kubectl exec -it postgres-74d79755d9-wjw6c -- psql -U postgres -d postgres
+
+kubectl exec -it postgres-77b9699b65-2bwfw -- psql -U postgres -d postgres -c "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;"
+kubectl exec -it postgres-dd9d647d8-rl476 -- psql -U postgres -d postgres -c "SELECT * FROM pg_stat_statements LIMIT 1;"
+
+kubectl exec -it postgres-5cfcfd6cff-7drpt -- psql -U postgres -d postgres -c "SHOW shared_preload_libraries;"
+kubectl exec -it postgres-5cfcfd6cff-7drpt -- psql -U postgres -d postgres -c "SELECT * FROM pg_stat_statements LIMIT 1;"
+
